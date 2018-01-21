64deefbb8fbb345cadbb60ed90fc0b12ad4e4b50
Jason Lutterloh <22846656+jasonlutterloh@users.noreply.github.com>
2018-01-21 14:17:56 -0600

    Removed unused dependency

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Steps from './components/Steps.js';
-import {SettingsButton, SettingsOverlay} from './components/Settings.js';
 import sequence from './sequences/sample.js';
 import './App.css';
 
@@ -9,31 +8,19 @@ class App extends Component {
     super(props);
     this.state = {
       title: sequence.title,
-      steps: sequence.steps,
-      isSettingsOpen: false
+      steps: sequence.steps
     }
-
-    //TODO: Implement settings. Uncomment HTML.
-    this.toggleSettings = this.toggleSettings.bind(this);
-  }
-
-  toggleSettings() {
-    this.setState({
-      isSettingsOpen: !this.state.isSettingsOpen
-    });
   }
 
   render() {
     return (
       <div>
-        {/*<SettingsButton action = {this.toggleSettings} /> */}
         <div className="app-container">
           <header>
             <h1 className="sequence-title">{this.state.title}</h1>
           </header>
           <Steps steps={this.state.steps} />
         </div>
-        {/*this.state.isSettingsOpen ? <SettingsOverlay></SettingsOverlay> : null */}
       </div>
     );
   }
