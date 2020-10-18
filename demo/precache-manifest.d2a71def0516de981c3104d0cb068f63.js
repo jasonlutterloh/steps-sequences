a1ab3c5de62e0131af9e069355a22223688a8d6a
jasonlutterloh <22846656+jasonlutterloh@users.noreply.github.com>
2020-10-18 15:54:29 -0500

    Adding demo to repo

---
+++ demo/precache-manifest.d2a71def0516de981c3104d0cb068f63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e441b7e7e3668a240e5a3294f5608b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e89a65cf20b1dbcf3c84",
+    "url": "/static/css/main.d6c2c49e.chunk.css"
+  },
+  {
+    "revision": "6970d45ae2e36701b46f",
+    "url": "/static/js/2.035cca82.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.035cca82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e89a65cf20b1dbcf3c84",
+    "url": "/static/js/main.8dd24f0d.chunk.js"
+  },
+  {
+    "revision": "9db6a87adf2a15cab2bb",
+    "url": "/static/js/runtime-main.2b4c6188.js"
+  }
+]);
